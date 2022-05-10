dcfb52cf908ecdbbe89ae78a5fc9460de8cc40ea
33012阿部 直樹 <33012@ojs.ac.jp>
2022-05-10 15:22:37 +0900

    問題2.1.3　完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,25 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+
+            //2.1.3
+            var songs = new Song[] {
+            new Song("Let it be","The Beatles",243),
+            new Song("CHRONICLE","Masato Okukai",211),
+            new Song("PREMIUM","Masato Okukai",209),
+            new Song("GRAND CROSS","Masato Okukai",200),
+            };
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) { 
+
+            foreach(var song in songs) {
+
+
+            }
         }
     }
 }
+
