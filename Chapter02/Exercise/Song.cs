30b5d8cc74ba02d39c83c972551ad1460c7f3bf6
33012阿部 直樹 <33012@ojs.ac.jp>
2022-05-10 14:04:36 +0900

    問題2.1.1完成

---
+++ Chapter02/Exercise/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    class Song {
+
+        //歌のタイトル
+        public string Title { get; set; }
+
+        //アーティスト名
+        public string ArtiseName { get; set; }
+
+        //演奏時間（単位：秒）
+        public int Length { get; set; }
+    }
+}
