671462c66762d481278780405d8d738c1e3339cb
33012阿部 直樹 <33012@ojs.ac.jp>
2022-05-10 15:54:07 +0900

    問題2.1.4　完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -8,21 +8,22 @@ namespace Exercise {
     class Program {
         static void Main(string[] args) {
 
-            //2.1.3
+        //2.1.3
+            
             var songs = new Song[] {
             new Song("Let it be","The Beatles",243),
-            new Song("CHRONICLE","Masato Okukai",211),
-            new Song("PREMIUM","Masato Okukai",209),
-            new Song("GRAND CROSS","Masato Okukai",200),
+            new Song("CHRONICLE","Team Fortune",211),
+            new Song("PREMIUM","Team Fortune",209),
+            new Song("PLUS","Team Fortune",206),
             };
             PrintSongs(songs);
         }
 
         //2.1.4
-        private static void PrintSongs(Song[] songs) { 
+        private static void PrintSongs(Song[] songs) {
 
             foreach(var song in songs) {
-
+                Console.WriteLine("{0},{1},{2:m\\:ss}", song.Title, song.ArtiseName, TimeSpan.FromSeconds(song.Length));
 
             }
         }
