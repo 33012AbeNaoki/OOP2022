350f660592c675e43f42cf4cb9a762213abe4248
33012阿部 直樹 <33012@ojs.ac.jp>
2022-05-26 15:32:27 +0900

    問題5-1終わり

---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("１つ目を入力：");
+            var inputString1 = Console.ReadLine();
+            Console.Write("２つ目を入力：");
+            var inputString2 = Console.ReadLine();
+           
+            if (string.Compare(inputString1, inputString2, ignoreCase: true)==0) {
+                Console.WriteLine("等しい");
+            }
+            else {
+                Console.WriteLine("等しくない");
+            }
+
+        }
+    }
+}
