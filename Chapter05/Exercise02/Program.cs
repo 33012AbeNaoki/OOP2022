24610af55aaf16755151215556f40258356e5da8
33012阿部 直樹 <33012@ojs.ac.jp>
2022-05-27 13:41:08 +0900

    問題5.2完成

---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("入力：");
+            var line = Console.ReadLine();
+
+            int num;//整数値を入れる変数を用意
+            if (int.TryParse(line, out num)) {
+                Console.WriteLine("{0:#,#}", num);//整数への変換成功
+            }
+            else {
+                Console.WriteLine("数字ではありません");//変換失敗
+            }
+            
+        }
+    }
+}
