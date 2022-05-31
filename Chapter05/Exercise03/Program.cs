f84f670047df9c4c1e02764a0e43a19a247e9742
33012阿部 直樹 <33012@ojs.ac.jp>
2022-05-31 13:36:04 +0900

    問題5.1～5.3完成

--- Chapter05/Exercise03/Program.cs
+++ Chapter05/Exercise03/Program.cs
@@ -27,25 +27,63 @@ namespace Exercise03 {
 
         private static void Exercise3_1(string text) {
 
-            Console.WriteLine(text.Where(t => t ==  ' ').Count());
+            int spase = text.Count(c => c == ' ');
+            Console.WriteLine("空白数：{0}",spase);
+
+            //Console.WriteLine(text.Count(t => t ==  ' '));
         }
 
         private static void Exercise3_2(string text) {
 
-            Console.WriteLine(text.Replace("big", "small"));
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+            
+            //Console.WriteLine(text.Replace("big", "small"));
         }
 
         private static void Exercise3_3(string text) {
-           
 
+            int count = text.Split(' ').Length;
+            Console.WriteLine("単語数：{0}",count);
+
+            //String[] words = text.Split(' ');
+            //Console.WriteLine("単語の数：" + words.Count());
         }
 
         private static void Exercise3_4(string text) {
-          
+
+            text.Split(' ').Where(word => word.Length <= 4).ToList().ForEach(word => Console.WriteLine(word));
+
+           //var s = text.Split(' ').Where(word => word.Length <= 4);
+
+           // foreach (var item in s) {
+           //     Console.WriteLine(item);
+           // }
+
+            //var words = text.Split(' ').Where(w => w.Length <= 4);
+
+            //foreach (var word in words) {
+                //Console.WriteLine(word);
+            //}
         }
 
         private static void Exercise3_5(string text) {
-           
+
+            var array = text.Split(' ').ToArray();
+            if (array.Length >0) {
+                var sb = new StringBuilder(array[0]);
+                foreach (var word in array.Skip(1)) {
+                    
+                    sb.Append(' ');
+                    sb.Append(word);
+                }
+                var str = sb.ToString();
+                Console.WriteLine(text);
+            }
+
+
+
+
         }
     }
 }
