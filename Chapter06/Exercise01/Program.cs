b9c5fc818a69ae0f4b320c31c6d6584c07a68bd1
33012阿部 直樹 <33012@ojs.ac.jp>
2022-06-03 13:11:57 +0900

    問題6.2完成

---
+++ Chapter06/Exercise01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+            //最大値
+
+            var max = numbers.Max();
+            Console.WriteLine(max);
+            
+            //Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+
+            //最後から2つの要素を取り出す
+
+            foreach (var n in numbers.Skip(numbers.Length - 2)) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+
+            //数値を文字列に変換（select）
+           var strs = numbers.Select(n => n.ToString());
+            foreach (var s in strs) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+
+            //小さい順に並べて先頭の３つを出力
+            foreach (var n in numbers.OrderBy(n => n).Take(3)) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+
+            //重複を排除し、１０より大きい数がいくつあるか
+            var count = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine(count);
+        }
+    }
+}
