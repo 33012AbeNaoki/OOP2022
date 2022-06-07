7bc3132c3d8cf32990aa17f66707b2452cf9edb0
33012阿部 直樹 <33012@ojs.ac.jp>
2022-06-07 15:41:52 +0900

    指定した範囲の乱数表示

--- FormAppSample/Sample0607/Form1.Designer.cs
+++ FormAppSample/Sample0607/Form1.Designer.cs
@@ -25,27 +25,69 @@ namespace Sample0607 {
         /// </summary>
         private void InitializeComponent() {
             this.btRondom = new System.Windows.Forms.Button();
+            this.udMax = new System.Windows.Forms.NumericUpDown();
+            this.udMin = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.number = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.udMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udMin)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.number)).BeginInit();
             this.SuspendLayout();
             // 
             // btRondom
             // 
-            this.btRondom.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btRondom.Location = new System.Drawing.Point(285, 57);
+            this.btRondom.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btRondom.Location = new System.Drawing.Point(255, 161);
             this.btRondom.Name = "btRondom";
-            this.btRondom.Size = new System.Drawing.Size(130, 39);
+            this.btRondom.Size = new System.Drawing.Size(146, 39);
             this.btRondom.TabIndex = 0;
             this.btRondom.Text = "乱数取得";
             this.btRondom.UseVisualStyleBackColor = true;
             this.btRondom.Click += new System.EventHandler(this.btRondom_Click);
             // 
+            // udMax
+            // 
+            this.udMax.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.udMax.Location = new System.Drawing.Point(264, 57);
+            this.udMax.Name = "udMax";
+            this.udMax.Size = new System.Drawing.Size(120, 42);
+            this.udMax.TabIndex = 1;
+            // 
+            // udMin
+            // 
+            this.udMin.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.udMin.Location = new System.Drawing.Point(89, 57);
+            this.udMin.Name = "udMin";
+            this.udMin.Size = new System.Drawing.Size(120, 42);
+            this.udMin.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(259, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "最大";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(84, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "最小";
+            // 
             // number
             // 
-            this.number.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.number.Location = new System.Drawing.Point(64, 57);
+            this.number.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.number.Location = new System.Drawing.Point(89, 156);
             this.number.Name = "number";
-            this.number.Size = new System.Drawing.Size(120, 42);
+            this.number.Size = new System.Drawing.Size(120, 44);
             this.number.TabIndex = 1;
             // 
             // Form1
@@ -53,19 +95,30 @@ namespace Sample0607 {
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(525, 145);
+            this.ClientSize = new System.Drawing.Size(502, 241);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.number);
+            this.Controls.Add(this.udMin);
+            this.Controls.Add(this.udMax);
             this.Controls.Add(this.btRondom);
             this.Name = "Form1";
             this.Text = "乱数アプリ";
+            ((System.ComponentModel.ISupportInitialize)(this.udMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udMin)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.number)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btRondom;
+        private System.Windows.Forms.NumericUpDown udMax;
+        private System.Windows.Forms.NumericUpDown udMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown number;
     }
 }
--- FormAppSample/Sample0607/Form1.cs
+++ FormAppSample/Sample0607/Form1.cs
@@ -11,13 +11,20 @@ using System.Windows.Forms;
 namespace Sample0607 {
     public partial class Form1 : Form {
 
-        Random rand = new Random();
+        Random rand = new Random();//乱数
+
         public Form1() {
             InitializeComponent();
         }
         private void btRondom_Click(object sender, EventArgs e) {
 
-            number.Value = rand.Next(minValue:1,maxValue:7);
+            //number.Value = rand.Next(minValue:1,maxValue:7); 1～6の乱数
+
+            number.Value = rand.Next(minValue:(int)udMin.Value,maxValue:(int)udMax.Value);
+        }
+        //イベントハンドラ（起動時に一度だけ実行）
+        private void Form_Loand(object sender, EventArgs e) { 
+        
         }
     }
 }
