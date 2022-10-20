1fef7bc79c0a3c6e91b421628c3c252fc87bbce2
33012阿部 直樹 <33012@ojs.ac.jp>
2022-10-20 15:57:16 +0900

    10/20 授業

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,13 +20,68 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            GetSeason();
+            
+;        }
+
+         public int GetSeason() {
+
+            var today = DateTime.Now;
+            int Month = today.Month; ;
+
+            switch (today.Month) {
+
+                case 11:
+                case 12:
+                case 1:
+                case 2:
+                    sesaonComboBox.SelectedIndex = 3;
+                    break;
+                case 4:
+                case 5:
+                    sesaonComboBox.SelectedIndex = 0;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    sesaonComboBox.SelectedIndex = 1;
+                    break;
+                case 9:
+                case 10:
+                    sesaonComboBox.SelectedIndex = 2;
+                    break;
+                    
+            }
+            return Month;
         }
 
-        private void okButton_Click(object sender, RoutedEventArgs e) {
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
 
         }
 
-        private void cancelButton_Click(object sender, RoutedEventArgs e) {
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+
+        }
+
+        private void redRudioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
+
+        }
+
+        private void yellowRudioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+
+        }
+
+        private void blueRudioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
+
+        }
+
+        private void sesaonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)sesaonComboBox.SelectedItem).Content;
 
         }
     }
