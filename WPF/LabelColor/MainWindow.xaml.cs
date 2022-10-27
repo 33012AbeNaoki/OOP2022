8987d553ba7019d01d9a0bec4371b6626af90bf6
33012阿部 直樹 <33012@ojs.ac.jp>
2022-10-27 11:49:49 +0900

    20221027 コンボボックスから色を変える

--- WPF/LabelColor/MainWindow.xaml.cs
+++ WPF/LabelColor/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,20 +22,70 @@ namespace ColorChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-           
+            DataContext = GetColorList();
+
+        }
+        /// <summary>
+        /// 色と色名を保持するクラス
+        /// </summary>
+        public class MyColor {
+            public Color Color { get; set; }
+            public string Name { get; set; }
+        }
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
 
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
 
 
-            int r = (int)rSlider.Value ;
-            int g = (int)gSlider.Value ;
-            int b = (int)bSlider.Value ;
+
+            int r = (int)rSlider.Value;
+            int g = (int)gSlider.Value;
+            int b = (int)bSlider.Value;
 
             cLavel.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+
         }
 
-       
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            var name = mycolor.Name;
+
+            cLavel.Background = new SolidColorBrush(Color.FromRgb(color.R, color.G, color.B));
+
+            rText.Text = FromRgb(color.R);
+            gText.Text = FromRgb(color.G);
+            bText.Text = FromRgb(color.B);
+        }
+        private string FromRgb(byte r) {
+
+            return r.ToString();
+
+        }
+
+        private void Slider_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+
+            e.Handled = !new Regex("[0-9]").IsMatch(e.Text);
+        }
+
+        private void Slider_PreviewExecuted(object sender, ExecutedRoutedEventArgs e) {
+
+            if (e.Command == ApplicationCommands.Paste) {
+                e.Handled = true;
+            }
+        }
+
+        private void Text_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+
+        }
     }
 }
