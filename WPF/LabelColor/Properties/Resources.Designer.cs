987e92ea485859f21aad598e50436b90db0dc83e
33012阿部 直樹 <33012@ojs.ac.jp>
2022-10-27 09:32:09 +0900

    20221027　RGBMAX値250→255

--- WPF/LabelColor/Properties/Resources.Designer.cs
+++ WPF/LabelColor/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace LabelColor.Properties {
+namespace ColorChecker.Properties {
     /// <summary>
     ///   ローカライズされた文字列などを検索するための、厳密に型指定されたリソース クラスです。
     /// </summary>
